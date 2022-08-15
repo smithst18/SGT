c6a342ae0463f735896efc60c2e017377ba9c3be
smithst <62660737+smithst18@users.noreply.github.com>
2022-08-15 14:49:56 -0400

    libreria para stadisticas graficas etc
    https://chartkick.com/vue

--- SGT-FRONT/sgt-front/src/main.js
+++ SGT-FRONT/sgt-front/src/main.js
@@ -3,10 +3,14 @@ import { createPinia } from "pinia";
 import "../src/css/index.css"
 import App from "./App.vue";
 import router from "./router";
+//charts y graficas
+import VueChartkick from 'vue-chartkick';
+import 'chartkick/chart.js';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueChartkick);
 
 app.mount("#app");
--- SGT-FRONT/sgt-front/src/modules/ticket/pages/AdminHome.vue
+++ SGT-FRONT/sgt-front/src/modules/ticket/pages/AdminHome.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="w-full h-full border border-cyan-900 p-10 grid grid-col-3 rounded-lg">
-    Aqui van las graficas
+    <div class="w-28 border border-cyan-500">
+      <pie-chart :data="[['Blueberry', 44], ['Strawberry', 23],['prueba',55]]"></pie-chart>
+    </div>
   </div>
 </template>
 
