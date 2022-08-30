123aef215e6780e85b511ef85846ee74703ee73f
smithst <62660737+smithst18@users.noreply.github.com>
2022-08-30 14:01:31 -0400

    tikcet store

---
+++ SGT-FRONT/sgt-front/src/modules/ticket/stores/ticketStore.js
@@ -0,0 +1,18 @@
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+//cookies package
+import { useCookies } from "vue3-cookies";
+
+// const localStorage = window.localStorage; 
+const { cookies } = useCookies();
+
+export const useTicketStore = defineStore('ticketStore', () => {
+  //store
+  
+  
+  //actions
+
+  //getters
+
+  return {}
+});
