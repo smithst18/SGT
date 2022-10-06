27af3d9e171ef3b5c156a5cf6f36731f8d410dc3
Emanuel <emanuelabreu72@gmail.com>
2022-10-06 12:29:45 -0400

    new login

--- SGT-FRONT/src/views/LoginView.vue
+++ SGT-FRONT/src/views/LoginView.vue
@@ -1,31 +1,58 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-full bg-primary">
-    <img src="@/assets/icons/logo.svg" alt="central +" class="pointer">
-    <div class="px-8 py-6 mt-4 text-left bg-white shadow-lg rounded-lg lg:w-2/6">
-        <h3 class="text-2xl font-bold text-center text-primary">Iniciar sesión</h3>
-        <form @submit.prevent="sendForm">
-            <div class="mt-4">
-                <div>
-                    <label class="block" for="user">Nombre de usuario</label>
-                            <input type="text" placeholder="Nombre de usuario" v-model="user.nickName"
-                                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
-                </div>
-                <div class="mt-4">
-                  <label class="block">Contraseña</label>
-                            <input type="password" placeholder="Contraseña" v-model="user.password"
-                                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
-                </div>      
-                <span v-if="errorlogin" class="text-xs tracking-wide text-red-600 block text-center mt-5">Usuario o Contraseña incorrectos </span>
-                <div class="flex items-baseline justify-between">
-                    <button 
-                      class="px-6 py-2 mt-4 text-white bg-primary rounded-lg hover:bg-slate-500 w-full">Entrar</button>
-                </div>
+  <div class="flex min-h-full">
+    <div class="w-1/2 img-background bg-no-repeat bg-cover"></div>
+    <div class="w-1/2 p-10">
+
+      <img src="@/assets/imgs/logo.png" alt="central +" class="pointer mx-auto mb-10">
+
+      <div class="px-8 py-6 mt-4 text-left lg:w-full">
+          <h3 class="text-2xl text-center text-primary mb-5">Login</h3>
+          <form @submit.prevent="sendForm" id="form">
+            <div class="relative z-0 w-full mb-10">
+              <input
+                type="text"
+                name="name"
+                required
+                placeholder=" "
+                v-model="user.nickName"
+                class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-primary border-gray-200"
+              />
+              <label for="name" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Nombre de usuario</label>
             </div>
-        </form>
+
+            <div class="relative z-0 w-full mb-10">
+              <input
+                type="password"
+                name="name"
+                required
+                placeholder=" "
+                v-model="user.password"
+                class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-primary border-gray-200"
+              />
+              <label for="name" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Contraseña</label>
+            </div>
+
+            <div class="text-center">
+              <span 
+              class="text-sm text-center text-red-600" 
+              id="error"
+              v-show="errorlogin">
+                Usuario / Contraseña invalidos
+              </span>
+            </div>
+            
+            <button 
+              type="submit"
+              class="px-6 py-2 mt-4 text-white bg-primary rounded-sm hover:bg-slate-500 w-full">
+              Iniciar Sesion
+            </button>
+          </form>
+      </div>
+
     </div>
+    
   </div>
 </template>
-
 <script setup>
   import { watch, inject, reactive, ref } from "vue";
   import { useMainStore } from "../stores/mainStore.js";
@@ -58,18 +85,53 @@
   const sendForm = async () =>{
     
     const { data, status } = await mainStore.logIn(user);
-    console.log(data);
 
     if(status){
       Toast.fire({ icon: 'success', title: 'Sesion Iniciada'});
-      router.push({name:'techHome'});
+      router.push({ name:'techHome' });
     }else{
-      Toast.fire({ icon: 'error', title: 'No se ha podido iniciar Sesion'});
+      Toast.fire({ icon: 'error', title: 'No se ha podido iniciar Sesion' });
       errorlogin.value = true;
     }
   }
 </script>
 
-<style>
+<style scoped>
+.img-background{
+  background-image: url("src/assets/imgs/img-logIn.jpg");
+}
+.-z-1 {
+    z-index: -1;
+  }
 
+  .origin-0 {
+    transform-origin: 0%;
+  }
+
+  input:focus ~ label,
+  input:not(:placeholder-shown) ~ label,
+  textarea:focus ~ label,
+  textarea:not(:placeholder-shown) ~ label,
+  select:focus ~ label,
+  select:not([value='']):valid ~ label {
+    /* @apply transform; scale-75; -translate-y-6; */
+    --tw-translate-x: 0;
+    --tw-translate-y: 0;
+    --tw-rotate: 0;
+    --tw-skew-x: 0;
+    --tw-skew-y: 0;
+    transform: translateX(var(--tw-translate-x)) translateY(var(--tw-translate-y)) rotate(var(--tw-rotate))
+      skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+    --tw-scale-x: 0.75;
+    --tw-scale-y: 0.75;
+    --tw-translate-y: -1.5rem;
+  }
+
+  input:focus ~ label,
+  select:focus ~ label {
+    /* @apply text-black; left-0; */
+    --tw-text-opacity: 1;
+    color: rgba(0, 0, 0, var(--tw-text-opacity));
+    left: 0px;
+  }
 </style>
