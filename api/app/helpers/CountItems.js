fc315c26ea65771fb52fa447d9dcc859a3390a2b
smithst <62660737+smithst18@users.noreply.github.com>
2023-07-25 07:52:30 -0400

    commit

--- api/app/helpers/CountItems.js
+++
@@ -1,7 +0,0 @@
-  //function that count n count of elements in an array
-export const countItems = (itemsArray) =>{
-  //return and object with the count of items
-  const count = itemsArray.reduce ( ( acc, el) => (acc [el] ? acc[el] += 1 : acc[el] = 1 , acc) , {});
-
-  return count
-}
