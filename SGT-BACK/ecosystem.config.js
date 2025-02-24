4788bee8abf9e98f4f40f57bdc3d7e5e8999b0d7
smithst <62660737+smithst18@users.noreply.github.com>
2025-02-24 15:35:08 -0400

    Create ecosystem.config.js

---
+++ SGT-BACK/ecosystem.config.js
@@ -0,0 +1,15 @@
+const apps = [
+  {
+    name: 'sgt',
+    script: 'app/server.js',
+    interpreter: './node_modules/.bin/babel-node',
+    env: {
+      PORT: "3002",
+      DB_URI: "mongodb://admin:0p3r4d0rm1np3sc4%2F%3F%2A@172.17.88.209:27017/emsdm?authSource=admin",
+      PRIVATE_KEY: "SgH78/?+_01As",
+      PUBLIC_URL: "http://sgt.pescalba.gob.ve:3002/public"
+    },
+  },
+];
+
+module.exports = { apps };
