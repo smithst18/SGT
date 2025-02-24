6ca59f589b3963db21a35626e88102c46edc2383
smithst <62660737+smithst18@users.noreply.github.com>
2025-02-24 15:35:49 -0400

    Update ecosystem.config.js

--- SGT-BACK/ecosystem.config.js
+++ SGT-BACK/ecosystem.config.js
@@ -4,10 +4,10 @@ const apps = [
     script: 'app/server.js',
     interpreter: './node_modules/.bin/babel-node',
     env: {
-      PORT: "3002",
+      PORT: "3200",
       DB_URI: "mongodb://admin:0p3r4d0rm1np3sc4%2F%3F%2A@172.17.88.209:27017/emsdm?authSource=admin",
       PRIVATE_KEY: "SgH78/?+_01As",
-      PUBLIC_URL: "http://sgt.pescalba.gob.ve:3002/public"
+      PUBLIC_URL: "http://sgt.pescalba.gob.ve:3200/public"
     },
   },
 ];
